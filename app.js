597c7d2610b0c9adb371fe0c4ac469ec95f77fae
Quang <nguyennhatquangkod2003@gmail.com>
2023-11-12 19:23:21 +0700

    Admin init
    + Add template files
    + Login page
    + Register
    + Dashboard

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+//Admin Routers
+var adminDashboardRouter = require('./components/admin/dashboard');
+var adminLoginRouter = require('./components/admin/login');
+var adminRegisterRouter = require('./components/admin/register');
 
 var app = express();
 
@@ -13,14 +20,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(expressLayouts)
+app.set('layout', './admin/layouts/layout')
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use("/public", express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin/dashboard', adminDashboardRouter);
+app.use('/admin/login', adminLoginRouter);
+app.use('/admin/register', adminRegisterRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
