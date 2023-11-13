450d5af5d23ac29b152c45a167f18ac7178aa76b
Tran_Minh_Khoa <tmkhoa21@clc.fitus.edu.vn>
2023-11-13 15:59:09 +0700

    router user-front-end and add products page

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var homeRouter = require('./components/user/home/home.router');
+var productsRouter = require('./components/user/product/product.router');
+var cartRouter = require('./components/user/cart/cart.router');
 
 var app = express();
 
@@ -19,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', homeRouter);
+app.use('/products', productsRouter);
+app.use('/cart', cartRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
