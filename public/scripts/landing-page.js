4f41ad3b3a3c95dc375aac82a649ce90b13ee4dc
PICKLE\hoang <hoanglam092003@gmail.com>
2023-11-23 11:13:59 +0700

    implemented image carousel for landing page and adjusted href links in various places

--- public/scripts/landing-page.js
+++ public/scripts/landing-page.js
@@ -29,3 +29,87 @@ quantityFields.forEach((item, i) => {
         item.value = value + 1 > 99 ? 99 : value + 1
     })
 })
+
+const images =  [...document.querySelectorAll('.slider-img')]
+const overlays = [...document.querySelectorAll('.overlay')]
+
+//Initialize image slider's indicators
+document.querySelector('.indicators').innerHTML = images.map((element, index) => {
+    return `<button type="button" class="indicator-btn" onclick="handleSlideTo(event, ${index})">${index + 1}</button>`
+}).join('')
+
+const indicators = [...document.querySelectorAll('.indicator-btn')]
+
+images[0].classList.add('img-appear')
+overlays[0].classList.add('overlay-appear')
+indicators[0].classList.add('indicator-btn-chosen')
+addClassToAllChildren(overlays[0], 'overlay-text-appear')
+
+i = 0;
+max_i = images.length
+
+function handleSlideTo(event, index) {
+    event.stopPropagation()
+    slideTo(index)
+}
+
+function slideTo(index) {
+    images[i].classList.remove('img-appear')
+    overlays[i].classList.remove('overlay-appear') 
+    indicators[i].classList.remove('indicator-btn-chosen')
+    removeClassToAllChildren(overlays[i], 'overlay-text-appear')
+    i = index
+    images[i].classList.add('img-appear')
+    overlays[i].classList.add('overlay-appear')
+    indicators[i].classList.add('indicator-btn-chosen')
+    addClassToAllChildren(overlays[i], 'overlay-text-appear')
+}
+
+function addClassToAllChildren(element, class_name){
+    children = element.children
+    let index = 0
+    const interval = 70
+
+    const addClass = () => {
+        if(index < children.length){
+            children[index].classList.add(class_name)
+            index++
+        }
+        else{
+            clearInterval(intervalLoop);
+        }
+    }
+
+    const intervalLoop = setInterval(addClass, interval)
+}
+
+function removeClassToAllChildren(element, class_name){
+    for (const child of element.children){
+        child.classList.remove(class_name)
+    }
+}
+
+//slide image based on cursor's position
+
+const image_container = document.querySelector('.image-container')
+
+image_container.addEventListener('click', (e) => {
+    const clickX = e.clientX - image_container.getBoundingClientRect().left
+    const divWidth = image_container.clientWidth
+
+    if(clickX < divWidth / 2){
+        slideTo( i - 1 < 0 ? max_i - 1 : i - 1)
+    }
+    else{
+        slideTo((i + 1 ) % max_i)
+    }
+})
+
+// setInterval(function() {
+//     image[i].classList.remove('img-appear')
+//     overlay[i].classList.remove('overlay-appear')
+//     i = (i +1) % 2
+//     overlay[i].classList.add('overlay-appear')
+//     image[i].classList.add('img-appear')
+
+// }, 2000)
