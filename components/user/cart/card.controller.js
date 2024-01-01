761637b4dc713484927374a9d3b91a614b6f826a
Tran-Minh-Khoa <99416417+Tran-Minh-Khoa@users.noreply.github.com>
2024-01-01 17:00:00 +0700

    fix bug

--- components/user/cart/card.controller.js
+++ components/user/cart/card.controller.js
@@ -1,3 +1,5 @@
+const Cart = require('../../../models/Cart');
+const Card = require('../../../models/Card');
 exports.CartPage = function(req, res, next) {
   const scripts = [
     '/scripts/cart.js"',
@@ -5,6 +7,18 @@ exports.CartPage = function(req, res, next) {
   const styles = [
     "/styles/cart.css"
   ];
+  // const cart = Cart.findOne({userId: req.user.id})
+  // const dataRender={
+  //   total:cart.totalPrice,
+  //   items:[]
+  // }
+  // cart.items.forEach(async (item)=>{
+  //   const card= await Card.findOne({id:item.productId})
+  //   const itemData={
+  //     tatalprice:item.priece,
+  //     amount:item.quantity,
+  //   }
+  // })
   res.render('user/cart-page', 
   {
     layout: 'user/layouts/layout', 
